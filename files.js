7cc104d1580dfef0259f2a72fbe727a2139dcf6b
Rayen Bakali <rayen.bakali@gmail.com>
2023-10-18 21:36:29 +0100

    files manipulation

--- files.js
+++ files.js
@@ -10,22 +10,37 @@ const fs = require("fs");
 
 //write files
 
-fs.writeFileSync("./docs/blog.txt", "salem", (err, data) => {
-  console.log("file was written");
-});
+// fs.writeFileSync("./docs/blog.txt", "salem", (err, data) => {
+//   console.log("file was written");
+// });
 
-fs.writeFileSync("./docs/blog2.txt", "salem2", (err, data) => {
-  console.log("file was written");
-});
+// fs.writeFileSync("./docs/blog2.txt", "salem2", (err, data) => {
+//   console.log("file was written");
+// });
 
 //directories
-if (!fs.existsSync("./assets")) {
-  fs.mkdir("./assets", (err) => {
+// if (!fs.existsSync("./assets")) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("created");
+//   });
+// } else {
+//   fs.rmdir("./assets", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("deleted ma king");
+//   });
+// }
+
+//delete files
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", (err) => {
     if (err) {
       console.log(err);
     }
-    console.log("done");
+    console.log("file deleted");
   });
 }
-
-//delete files
